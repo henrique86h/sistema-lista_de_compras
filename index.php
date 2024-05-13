426e3ec1f9af0e55d546cf828fc66a68ed63e202
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-05-13 20:08:23 -0300

    .

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="reset.css">
   <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="admin.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="icon" href="img/icone-serenatto.png" type="image/x-icon">
